d0ca7131a563700f51f7cabf785099b1bea0ae12
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-11-06 23:03:47 +0200

    add/feature task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-
+const container = document.querySelector('.gallery');
+const markup = galleryItems.reduce(
+    (acc, { original, preview, description }) =>
+      (acc += `<li>
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`),
+    ''
+  );
+  
+  container.insertAdjacentHTML('beforeend', markup);
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 console.log(galleryItems);
