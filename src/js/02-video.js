3600e922edd3bc4eec3b7683065cebfafcc5024e
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-11-07 00:44:03 +0200

    add/feature task 2. not working!!..

---
+++ src/js/02-video.js
@@ -0,0 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// const onPlay = function ({ seconds }) {
+//   localStorage.setItem('videoplayer-current-time', `${seconds}`);
+// };
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+// player.setCurrentTime(currentTime);
+
+const player = new Player('vimeo-player', {
+    id: 236203659,
+    width: 640,
+});
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+
+function onPlayerTimeUpdate(event) {
+    localStorage.setItem('videoplayer-current-time', `${event.seconds}`)
+};
+
+const resumePlayback = localStorage.getItem('videoplayer-current-time');
+if (resumePlayback) {
+player.setCurrentTime(resumePlayback);}
